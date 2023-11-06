bf76c18ae12410ae70819e6fbc2f4fde352cc17f
krithikamayuri <mayurikrithika@gmail.com>
2023-11-06 13:52:57 +0800

    generate reports fixes
    fixed an issue with the pdf generation

--- src/boundary/EmployeeReport.js
+++ src/boundary/EmployeeReport.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getHoursWorked } from '../controller/shiftController';
 import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 //method to calculate the hours worked for each employee according to shifts
 const calculateHoursWorked = (startTime, endTime) => {
@@ -57,41 +58,64 @@ const EmployeeDetails = ({ employeeName }) => {
     fetchData();
   }, [employeeName]);
 
+  //method to generate pdf of report generated
   const handleDownloadPDF = () => {
     const doc = new jsPDF();
-  
+
+    // Add the employee's name to the PDF
+    doc.text(`${employeeName}'s Shifts`, 10, 10);
+
     // Create the PDF content
     if (Array.isArray(shiftInfo)) {
-      shiftInfo.forEach((shift, index) => {
-        const yCoordinate = 10 + index * 60; // Adjust the Y-coordinate for spacing
+      const shiftsData = [];
+      
+      shiftInfo.forEach((shift) => {
         const formattedDate = new Date(shift.shift_date).toLocaleDateString();
-  
-        doc.text(`Shift ID: ${shift.shift_id}`, 10, yCoordinate);
-        doc.text(`Date of Shift: ${formattedDate}`, 10, yCoordinate + 10);
-        doc.text(`Shift Start Time: ${shift.shift_start}`, 10, yCoordinate + 20);
-        doc.text(`Shift End Time: ${shift.shift_end}`, 10, yCoordinate + 30);
-        doc.text(`Number of hours worked: ${calculateHoursWorked(shift.shift_start, shift.shift_end)} hours`, 10, yCoordinate + 40);
-        // Add other shift details as needed
+        const hoursWorked = calculateHoursWorked(shift.shift_start, shift.shift_end);
+
+        // Push shift data to the table as an array
+        shiftsData.push([shift.shift_id, formattedDate, shift.shift_start, shift.shift_end, `${hoursWorked} hours`]);
+      });
+
+      // Define table headers
+      const headers = ['Shift ID', 'Date of Shift', 'Shift Start Time', 'Shift End Time', 'Hours Worked'];
+
+      // Create a table with headers and data
+      doc.autoTable({
+        head: [headers],
+        body: shiftsData,
+        startY: 10 // Adjust the Y-coordinate for the table
       });
     } else if (shiftInfo) {
-      // Handle the case when shiftInfo is not an array
-      doc.text(`Shift ID: ${shiftInfo.shift_id}`, 10, 10);
-      doc.text(`Date of Shift: ${shiftInfo.shift_date}`, 10, 20);
-      doc.text(`Shift Start Time: ${shiftInfo.shift_start}`, 10, 30);
-      doc.text(`Shift End Time: ${shiftInfo.shift_end}`, 10, 40);
-      doc.text(`Number of hours worked: ${calculateHoursWorked(shiftInfo.shift_start, shiftInfo.shift_end)} hours`, 10, 50);
-      // Add other shift details as needed
+      // Handle the case when shiftInfo is not an array (so only one shift returned from db)
+      const formattedDate = new Date(shiftInfo.shift_date).toLocaleDateString();
+      const hoursWorked = calculateHoursWorked(shiftInfo.shift_start, shiftInfo.shift_end);
+
+      const data = [[shiftInfo.shift_id, formattedDate, shiftInfo.shift_start, shiftInfo.shift_end, `${hoursWorked} hours`]];
+      
+      // Define table headers
+      const headers = ['Shift ID', 'Date of Shift', 'Shift Start Time', 'Shift End Time', 'Hours Worked'];
+
+      // Create a table with headers and data
+      doc.autoTable({
+        head: [headers],
+        body: data,
+        startY: 10 // Adjust the Y-coordinate for the table
+      });
     }
-  
+
     // Generate the PDF file name with the employee's name
     const employeeNameForFileName = employeeName.replace(/\s+/g, '_');
     const pdfFileName = `${employeeNameForFileName}_shifts.pdf`;
 
     doc.save(pdfFileName);
   };
-  
-  
 
+  
+  
+  
+  
+  //display the report generated for each employee as a table
   return (
     <div>
       <h4 style={{ textAlign: 'center' }}>Shift Information for {employeeName}</h4>
