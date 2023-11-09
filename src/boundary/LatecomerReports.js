9f197b5af3d7984a5695bd58347cc59920d27520
krithikamayuri <mayurikrithika@gmail.com>
2023-11-09 16:33:14 +0800

    reverting changes

--- src/boundary/LatecomerReports.js
+++ src/boundary/LatecomerReports.js
@@ -5,7 +5,6 @@ import AddEmployee from './AddEmployee';
 import ShiftCancel from './processShiftCancel';
 import { useState } from 'react';
 import ManagerDashboard from './managerDashboard';
-import { compareTimings } from '../controller/reportController';
 
 function LatecomerReports() {
   const [reportButtonPressed, setReportButtonPressed] = useState(false);
@@ -14,11 +13,6 @@ function LatecomerReports() {
   const [employeeButtonPressed, setEmployeeButtonPressed] = useState(false);
   const [lateButtonPressed, setLateButtonPressed] = useState(false);
   const [mgerButtonPressed, setMgerButtonPressed] = useState(false);
-  const [empId, setEmpId] = useState(''); // Initialize empId as an empty string
-  const [date, setDate] = useState(''); // Initialize date as an empty string
-  const [reportData, setReportData] = useState(null);
-  const [error, setError] = useState(null);
-  const [lateShiftsCount, setLateShiftsCount] = useState(0);
 
   const handleReportButtonClick = () => {
     // Set the state to indicate that the button has been pressed
@@ -62,29 +56,6 @@ function LatecomerReports() {
     setMgerButtonPressed(true);
   }
 
-  const handleEmpIdChange = (event) => {
-    setEmpId(event.target.value);
-  };
-
-  const handleDateChange = (event) => {
-    setDate(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    // Fetch data when the user submits the form
-    compareTimings(empId, date)
-      .then((data) => {
-        setReportData(data);
-
-        if (data && data.message === 'Clock-in time is later than shift start time') {
-          setLateShiftsCount((count) => count + 1);
-        }
-      })
-      .catch((err) => {
-        setError(err);
-      });
-  };
-
   return (
     <>
       {assignButtonPressed ? (
@@ -112,32 +83,7 @@ function LatecomerReports() {
           </div>
           <h1>View Clock-In Reports</h1>
           <p>Please select below</p>
-          <form>
-            <label>
-              Employee ID:
-              <input type="text" value={empId} onChange={handleEmpIdChange} />
-            </label>
-          <br />
-            <label>
-              Date:
-              <input type="text" value={date} onChange={handleDateChange} />
-            </label>
-          <br />
-            <button type="button" onClick={handleSubmit}>
-            Submit
-            </button>
-        </form>
-
-      {error && <p>Error: {error.message}</p>}
-
-      {reportData && (
-        <div>
-          <p>Report Data:</p>
-          <pre>{JSON.stringify(reportData, null, 2)}</pre>
-        </div>
-      )}
-
-      <p>Number of Shifts Late: {lateShiftsCount}</p>
+          
         </div>
       )}
     </>
--- src/controller/reportController.js
+++ src/controller/reportController.js
@@ -10,22 +10,3 @@ export const getEmpName = async () => {
         throw error;
     }
 };
-
-export const compareTimings = async (empId, date) => {
-    try {
-        const response = await axios.get(`/api/findLateTime/${empId}/${date}`);
-        console.log('API Response:', response.data); // Log the response data
-        return response.data;
-    } catch (error) {
-        console.error('API Error:', error); // Log any errors
-        throw error;
-    }
-};
-
-export const fetchEmpIdAndDate = async () => {
-    return fetch('/api/getEmpIdAndDate')
-      .then((response) => response.json())
-      .catch((err) => {
-        throw new Error('Error fetching empId and date: ', err);
-      });
-  };
