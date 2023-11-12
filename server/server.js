217440294a24a1e9af85870a36d4f2b717068775
ribenaa <144659319+ribenaa@users.noreply.github.com>
2023-11-13 02:31:06 +0800

    Update

--- server/server.js
+++ server/server.js
@@ -1226,16 +1226,14 @@ app.post('/api/clockinout', async (req, res) => {
     }
   });
 
+  // Serve static files from the 'build' folder in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../build')));
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    });
+  }
   
-  
-
-
-  
-
-  
-
-
-
 app.listen(5000, () => {
     console.log("Server started on port 5000")
 })
