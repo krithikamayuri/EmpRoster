060aa494f9f4fe284f3bd91958a644dafa1580e4
yhwong020 <yhwong020@hotmail.com>
2023-11-16 19:36:20 +0800

    small update on spelling

--- src/boundary/empdashboardnav.jsx
+++ src/boundary/empdashboardnav.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import nothing from "../images/notfound.jpg"
+import nothing from "../images/notfound.jpg";
 
 function Empdashboardnav(props){
 console.log(props.employeeId)
@@ -47,7 +47,7 @@ const formatDate = (dateString) => {
 
         <nav className="navbar navbar-expand-lg" style ={{background: 'linear-gradient(to bottom, #AA8B56, #876445)', marginTop: '0px', borderBottom: '2px solid black'}}>
           <div className="container-fluid">
-            <Link className="navbar-brand text-light" to="/availabilitysub">Calender</Link>
+            <Link className="navbar-brand text-light" to="/availabilitysub">Calendar</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
