d2e162ae48bfe500f430314053e45f5487ad44a3
Zby <yingyangyeo@gmail.com>
2023-11-17 22:51:38 +0800

    server.js clean up

--- server/server.js
+++ server/server.js
@@ -22,212 +22,14 @@ const CancelShiftRequest = require("../src/entity/shiftcancelrequests");
 const sequelize = require("../src/sequelize");
 const { Op } = require('sequelize');
 
-const app = express()
-
+const app = express();
 app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
 
 const { exec } = require('child_process');
-const bodyParser = require('body-parser');
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200, // For legacy browser support
-};
-
-// app.use(cors())
-app.use(cors(corsOptions));
-app.use(express.json({ limit: '10mb' }));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 
-
-// app.get('/', (req, res) => {
-//     res.render('index.html');
-// })
-
-// app.post('/oauth2callback', (req, res) => {
-//     const tkn = req.body.token;
-//     const fs = require('fs');
-//     const { google } = require('googleapis');
-
-//     const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-//     // The file token.json stores the user's access and refresh tokens, and is
-//     // created automatically when the authorization flow completes for the first
-//     // time.
-//     const TOKEN_PATH = 'token.json';
-
-//     // Load client secrets from a local file.
-//     fs.readFile('credentials.json', (err, content) => {
-//         if (err) return console.log('Error loading client secret file:', err);
-//         // Authorize a client with credentials, then call the Google Calendar API.
-//         authorize(JSON.parse(content), listEvents);
-//     });
-
-//     /**
-//      * Create an OAuth2 client with the given credentials, and then execute the
-//      * given callback function.
-//      * @param {Object} credentials The authorization client credentials.
-//      * @param {function} callback The callback to call with the authorized client.
-//      */
-//     function authorize(credentials, callback) {
-//         const { client_secret, client_id, redirect_uris } = credentials.installed;
-//         const oAuth2Client = new google.auth.OAuth2(
-//             client_id, client_secret, redirect_uris[0]);
-
-//         // Check if we have previously stored a token.
-//         fs.readFile(TOKEN_PATH, (err, token) => {
-//             if (err) return getAccessToken(oAuth2Client, callback);
-//             oAuth2Client.setCredentials(JSON.parse(token));
-//             callback(oAuth2Client);
-//         });
-//     }
-
-//     /**
-//      * Get and store new token after prompting for user authorization, and then
-//      * execute the given callback with the authorized OAuth2 client.
-//      * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
-//      * @param {getEventsCallback} callback The callback for the authorized client.
-//      */
-//     function getAccessToken(oAuth2Client, callback) {
-//         oAuth2Client.getToken(tkn, (err, token) => {
-//             if (err) return console.error('Error retrieving access token', err);
-//             oAuth2Client.setCredentials(token);
-//             // Store the token to disk for later program executions
-//             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-//                 if (err) return console.error(err);
-//                 console.log('Token stored to', TOKEN_PATH);
-//             });
-//             callback(oAuth2Client);
-//         });
-//     }
-
-//     /**
-//      * Lists the next events on the user's primary calendar.
-//      * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
-//      */
-//     function listEvents(auth) {
-//         async function fun() {
-//             const calendar = await google.calendar({ version: 'v3', auth });
-//             calendar.events.list({
-//                 calendarId: 'primary',
-//                 timeMin: (new Date()).toISOString(),
-//                 maxResults: 30,
-//                 singleEvents: true,
-//                 orderBy: 'startTime',
-//             }, (err, res) => {
-//                 if (err) return console.log('The API returned an error: ' + err);
-//                 const events = res.data.items;
-//                 if (events.length) {
-//                     console.log('Your upcoming events:', events);
-//                     events.map((event, i) => {
-//                         var_arr.push(event);
-//                     });
-//                 } else {
-//                     console.log('No upcoming events found.');
-//                 }
-//             });
-//         }
-//         fun()
-//     }
-//     res.send(var_arr)
-//     res.render('index.html')
-// });
-
-
-// app.post('/events', (req, res) => {
-//     // Require google from googleapis package.
-//     const { google } = require('googleapis')
-//     // Require oAuth2 from our google instance.
-//     const { OAuth2 } = google.auth
-
-//     // Create a new instance of oAuth and set our Client ID & Client Secret.
-//     const oAuth2Client = new OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
-
-//     // Call the setCredentials method on our oAuth2Client instance and set our refresh token.
-//     oAuth2Client.setCredentials({
-//         refresh_token: process.env.REFRESH_TOKEN,
-//     })
-
-//     // Create a new calender instance.
-//     const calendar = google.calendar({ version: 'v3', auth: oAuth2Client })
-
-//     // Create a new event start date instance for temp uses in our calendar.
-//     const eventStartTime = new Date()
-//     eventStartTime.setDate(eventStartTime.getDay() + 2)
-
-//     // Create a new event end date instance for temp uses in our calendar.
-//     const eventEndTime = new Date()
-//     eventEndTime.setDate(eventEndTime.getDay() + 2)
-//     eventEndTime.setMinutes(eventEndTime.getMinutes() + 60)
-
-//     // Create a dummy event for temp uses in our calendar
-//     const event = {
-//         summary: `${req.body.summary}`,
-//         description: `${req.body.description}`,
-//         colorId: 6,
-//         start: {
-//             dateTime: eventStartTime,
-//         },
-//         end: {
-//             dateTime: eventEndTime,
-//         },
-//     }
-
-//     // Check if we a busy and have an event on our calendar for the same time.
-//     calendar.freebusy.query(
-//         {
-//             resource: {
-//                 timeMin: eventStartTime,
-//                 timeMax: eventEndTime,
-//                 items: [{ id: 'primary' }],
-//             },
-//         },
-//         (err, res) => {
-//             // Check for errors in our query and log them if they exist.
-//             if (err) return console.error('Free Busy Query Error: ', err)
-
-//             // Create an array of all events on our calendar during that time.
-//             const eventArr = res.data.calendars.primary.busy
-
-//             // Check if event array is empty which means we are not busy
-//             if (eventArr.length === 0) {
-//                 // If we are not busy create a new calendar event.
-//                 return calendar.events.insert(
-//                     { calendarId: 'primary', resource: event },
-//                     err => {
-//                         // Check for errors and log them if they exist.
-//                         if (err) return console.error('Error Creating Calender Event:', err)
-//                         // Else log that the event was created.
-//                         return console.log('Event created successfully.')
-//                     })
-//             }
-//             // If event array is not empty log that we are busy.
-//             return console.log(`Sorry I'm busy for that time...`)
-//         }
-//     )
-//     console.log(req.body)
-//     const sgMail = require('@sendgrid/mail')
-//     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-//     const msg = {
-//         to: req.body.to, // Change to your recipient
-//         from: 'your email goes here', // Change to your verified sender
-//         subject: req.body.summary,
-//         text: req.body.description,
-//         html: req.body.description,
-//     }
-//     sgMail
-//         .send(msg)
-//         .then(() => {
-//             console.log('Email sent')
-//         })
-//         .catch((error) => {
-//             console.error(error)
-//         })
-
-//     res.render('events.html')
-// });
+app.use(cors())
+app.use(express.json());
 
 app.post('/api/calendar/fetch-events/:empId', async (req, res) => {
     try {
@@ -258,21 +60,23 @@ app.post('/api/calendar/fetch-events/:empId', async (req, res) => {
 });
 
 
-app.post('/api/calendar/fetch-events/:shiftID', async (req, res) => {
+app.post('/api/calendar/fetch-shift/:shiftID', async (req, res) => {
     try {
         console.log('Received POST request to /calendar/fetch-events');
         // Query the database to retrieve shift data
         let reqParam = req.params.shiftID;
 
         // Use include to fetch associated Employee data
-        const shifts = await Shift.findByPk(reqParam, {
-            include: [{ model: Employee }],
+        const shifts = await Shift.findAll({
+            where: {
+                shiftID: reqParam
+            }
         });
 
         const events = shifts.map((shift) => {
             return {
                 id: shift.shiftID, // Include the ID
-                name: shift.Employee.emp_name, // Access emp_name through the associated Employee model
+                name: shift.emp_name, // Access emp_name through the associated Employee model
                 date: shift.shiftDate,
                 start: shift.shiftStartTime,
                 end: shift.shiftEndTime,
@@ -720,8 +524,8 @@ app.post('/api/assign-employees', async (req, res) => {
                 staffRequired,
                 startTime,
                 endTime,
-          customerName,
-          customerAddress
+                customerName,
+                customerAddress
             });
         }));
 
@@ -858,7 +662,7 @@ app.get('/api/shiftTimings', async (req, res) => {
 
         // Convert the Sequelize instances to the correct format
         shiftTimings.forEach((shiftTiming) => {
-            const { date, startTime, endTime, customerName, customerAddress} = shiftTiming;
+            const { date, startTime, endTime, customerName, customerAddress } = shiftTiming;
             shiftTimingsFormatted[date] = [startTime, endTime, customerName, customerAddress];
         });
 
@@ -1472,11 +1276,11 @@ app.get('/api/fetchClockInOut', async (req, res) => {
     }
 });
 
-  app.get('/api/notifyshiftemployee/:empId', async (req, res) => {
+app.get('/api/notifyshiftemployee/:empId', async (req, res) => {
     const today = new Date();
     const twoDaysAhead = new Date(today);
     twoDaysAhead.setDate(today.getDate() + 2);
-    
+
     const threeDaysAhead = new Date(today);
     threeDaysAhead.setDate(today.getDate() + 3);
 
@@ -1505,8 +1309,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, '../build', 'index.html'));
     });
 }
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("Server started on port " + PORT)
+    console.log("Server started on port 5000")
 })
