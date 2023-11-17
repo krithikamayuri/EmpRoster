86b5fc553658f25dd31b2a295a10accbb0feab08
ribenaa <144659319+ribenaa@users.noreply.github.com>
2023-11-18 07:40:50 +0800

    unit tests

--- test/myswaprequests.test.js
+++ test/myswaprequests.test.js
@@ -1,97 +1,6 @@
-/*
-import React from 'react';
-import { shallow } from 'enzyme';
-import { act } from 'react-dom/test-utils';
-import MySwapRequests from '../src/boundary/myswaprequests';
-import {
-  getSwapShiftRequestsAsync,
-  updateSwapShiftRequest,
-  getEmployeeByEmailAsync
-} from '../src/controller/employeeDashboardController';
-
-jest.mock('../src/controller/employeeDashboardController');
-
-describe('MySwapRequests Component', () => {
-  let wrapper;
-  const empEmail = 'test@example.com';
-  const employeeId = 123;
-
-  beforeEach(() => {
-    wrapper = shallow(<MySwapRequests empEmail={empEmail} employeeId={employeeId} />);
-  });
-
-  it('should render without errors', () => {
-    expect(wrapper.exists()).toBe(true);
-  });
-
-  it('should handle request status change - accepted', async () => {
-    // Mock API response for getSwapShiftRequestsAsync
-    const getSwapShiftRequestsResponse = {
-      status: true,
-      requests_sent: [],
-      requests_received: [],
-    };
-    getSwapShiftRequestsAsync.mockResolvedValue(getSwapShiftRequestsResponse);
-
-    // Mock API response for updateSwapShiftRequest
-    const updateSwapShiftRequestResponse = {
-      status: true,
-    };
-    updateSwapShiftRequest.mockResolvedValue(updateSwapShiftRequestResponse);
-
-    // Mock API response for getEmployeeByEmailAsync
-    const getEmployeeResponse = {
-      success: true,
-      employee: {
-        emp_id: 456,
-        emp_name: 'John Doe',
-      },
-    };
-    getEmployeeByEmailAsync.mockResolvedValue(getEmployeeResponse);
-
-    // Trigger the useEffect
-    await act(async () => {
-      // You may need to adjust the timeout based on your component's async behavior
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      wrapper.update();
-    });
-
-    // Simulate button click to open modal
-    const modalBtn = wrapper.find('button[data-bs-target="#exampleModal"]');
-    modalBtn.simulate('click');
-
-    // Simulate accepting the request
-    await act(async () => {
-      const acceptBtn = wrapper.find('button.btn-success');
-      acceptBtn.simulate('click');
-    });
-
-    // Trigger the useEffect again after the modal action
-    await act(async () => {
-      // You may need to adjust the timeout based on your component's async behavior
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      wrapper.update();
-    });
-
-    // Assertions
-    expect(getSwapShiftRequestsAsync).toHaveBeenCalledWith(employeeId);
-    expect(updateSwapShiftRequest).toHaveBeenCalledWith({
-      swapReqId: expect.any(Number),
-      status: 'accepted',
-      shiftId: expect.any(Number),
-      requestedTo: 456,
-      emp_name: 'John Doe',
-    });
-    expect(wrapper.find('.alert-success').text()).toBe('Request Updated Successfully');
-  });
-});
-*/
-
-
 import React from 'react';
 import { shallow } from 'enzyme';
 import MySwapRequests from '../src/boundary/myswaprequests';
-//import * as controller from '../src/controller/employeeDashboardController';
 
 jest.mock('../src/controller/employeeDashboardController', () => ({
   getSwapShiftRequestsAsync: jest.fn(),
@@ -110,5 +19,21 @@ describe('MySwapRequests Component', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  
+  it('renders the table when requests are loaded', () => {
+    const receivedData = [{ swapReqId: 1, date: '2023-01-01', status: 'pending', requestedByName: 'John' }];
+    const wrapper = shallow(<MySwapRequests empEmail="test@example.com" employeeId={123} isLoading={false} received={receivedData} />);
+    expect(wrapper.find('table')).toHaveLength(1);
+  });
+
+  it('renders rejection button when request status is pending', () => {
+    const receivedData = [{ swapReqId: 1, date: '2023-01-01', status: 'pending', requestedByName: 'John', message: 'Test message' }];
+    const wrapper = shallow(<MySwapRequests empEmail="test@example.com" employeeId={123} received={receivedData} />);
+    expect(wrapper.find('.btn-danger')).toHaveLength(1);
+  });
+
+  it('renders acceptance button when request status is pending', () => {
+    const receivedData = [{ swapReqId: 1, date: '2023-01-01', status: 'pending', requestedByName: 'John', message: 'Test message' }];
+    const wrapper = shallow(<MySwapRequests empEmail="test@example.com" employeeId={123} received={receivedData} />);
+    expect(wrapper.find('.btn-success')).toHaveLength(1);
+  });
 });
