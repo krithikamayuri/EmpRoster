2d298f0b19fdb163a42b4e885691b42dafb5a272
ribenaa <144659319+ribenaa@users.noreply.github.com>
2023-11-20 22:05:21 +0800

    Update sequelize

--- src/sequelize.js
+++ src/sequelize.js
@@ -2,10 +2,11 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
-  host: 'localhost',
-  username: 'root',
-  password: 'test',
-  database: 'emproster',
+  host: 'dpg-clc9rpug1b2c73esubrg-a',
+  username: 'empdb_user',
+  password: 'ewlrYZ0rJoQFOqtduK5DboGRpvnd9b11',
+  database: 'empdb',
+  port: 5432
 });
 
 module.exports = sequelize;
